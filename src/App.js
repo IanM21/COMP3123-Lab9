4ddbfd1d26f8af60b3a60a312d7b038d650266e1
IanM21 <ianmcdonad2003@gmail.com>
2024-10-31 18:02:31 -0400

    Lab 9

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to Fullstack Development - I
+        </h1>
+        <h3>
+          React JS Programming Week09 Lab Exercise
+        </h3>
+        <p>
+          101419262
+        </p>
+        <p>
+          Ian McDonald
+        </p>
+        <p>
+          George Brown College, Toronto
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
